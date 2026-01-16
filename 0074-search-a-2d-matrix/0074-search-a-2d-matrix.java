b34290aa4478ed9143d4c9c239972f74628a6eb1
PAWAR SUDHARSHAN <pawarsudharshan47@gmail.com>
2026-01-16 09:31:49 +0530

    Time: 0 ms (100%), Space: 44.1 MB (29.26%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -2,15 +2,24 @@ import java.util.*;
 
 class Solution {
     public boolean searchMatrix(int[][] mat, int target) {
-        for (int[] row : mat) {
-            List<Integer> list = new ArrayList<>(row.length);
-            for (int x : row) {
-                list.add(x);
-            }
+        int n = mat.length;        // number of rows
+        int m = mat[0].length;     // number of columns
 
-            int idx = Collections.binarySearch(list, target);
-            if (idx >= 0) {  
+        int left = 0;
+        int right = n * m - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            int row = mid / m;     // use m here
+            int col = mid % m;     // and here
+
+            if (mat[row][col] == target) {
                 return true;
+            } else if (mat[row][col] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
         return false;
