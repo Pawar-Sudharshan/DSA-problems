655b43a3adbef9f250a2f0e305c57e2d6969a8cb
PAWAR SUDHARSHAN <pawarsudharshan47@gmail.com>
2026-02-22 13:23:31 +0530

    Time: 1 ms (40.87%), Space: 42.4 MB (30.35%) - LeetHub

---
+++ 0868-binary-gap/0868-binary-gap.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int binaryGap(int n) {
+        String s = Integer.toBinaryString(n);
+        int prev = -1;     
+        int ans = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                if (prev != -1) {
+                    ans = Math.max(ans, i - prev);
+                }
+                prev = i;
+            }
+        }
+
+        return ans;
+    }
+}
