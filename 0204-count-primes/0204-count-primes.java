936a379ec4d9f272f75f28ff050a9a91e1870d3d
PAWAR SUDHARSHAN <pawarsudharshan47@gmail.com>
2026-02-25 17:09:19 +0530

    Time: 94 ms (82.8%), Space: 51.6 MB (13.1%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int countPrimes(int n) {
+        if (n <= 2) return 0;
+
+        boolean[] prime = new boolean[n];
+        Arrays.fill(prime, true);
+        prime[0] = prime[1] = false;
+
+        // stop at i * i < n without overflow
+        for (int i = 2; i * i < n; i++) {          // condition moved here
+            if (prime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    prime[j] = false;
+                }
+            }
+        }
+
+        int cnt = 0;
+        for (int i = 2; i < n; i++) {
+            if (prime[i]) cnt++;
+        }
+        return cnt;
+    }
+}
