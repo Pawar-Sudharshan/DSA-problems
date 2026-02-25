2056b64f7d2a21ded26580a1ed1682cedb656154
PAWAR SUDHARSHAN <pawarsudharshan47@gmail.com>
2026-02-25 16:48:44 +0530

    Time: 11 ms (35.25%), Space: 46.9 MB (39.38%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
@@ -1,35 +1,28 @@
 class Solution {
-    private int countBits(int n) {
-        int cnt = 0;
-        while (n != 0) {
-            cnt += (n & 1);
-            n >>= 1;
-        }
-        return cnt;
-        // or: return Integer.bitCount(n);
-    }
 
     public int[] sortByBits(int[] arr) {
-        // key: count of set bits, value: all numbers with that count
-        TreeMap<Integer, List<Integer>> map = new TreeMap<>();
+        Map<Integer, List<Integer>> map = new TreeMap<>();
 
         for (int x : arr) {
-            int bits = countBits(x);
-            map.computeIfAbsent(bits, k -> new ArrayList<>()).add(x);
+            int bc = Integer.bitCount(x);
+            List<Integer> list = map.getOrDefault(bc, new ArrayList<>());
+            list.add(x);                
+            map.put(bc, list);          
         }
-
-        // for same bit-count, sort by value
         for (List<Integer> list : map.values()) {
             Collections.sort(list);
         }
 
-        // flatten back to array
-        int i = 0;
-        for (Map.Entry<Integer, List<Integer>> e : map.entrySet()) {
-            for (int v : e.getValue()) {
-                arr[i++] = v;
+        int n = arr.length;
+        int[] ans = new int[n];
+        int idx = 0;
+
+        for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
+            for (int x : entry.getValue()) {
+                ans[idx++] = x;
             }
         }
-        return arr;
+
+        return ans;
     }
 }
