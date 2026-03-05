450389e008ed8bb8a3afc7938aa32c7deb858232
PAWAR SUDHARSHAN <pawarsudharshan47@gmail.com>
2026-03-05 18:10:10 +0530

    Time: 4 ms (75.73%), Space: 43.7 MB (79.17%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -1,23 +1,13 @@
 class Solution {
     public int minOperations(String s) {
-        int n =s.length();
-        StringBuffer first = new StringBuffer();
-        StringBuffer second = new StringBuffer();
-        for(int i = 1 ; i <=n;i++ ){
-            // first = first<<1;
-            if(i%2 == 1) first.append('1');
-            else first.append('0');
+        int n = s.length();
+        int cnt = 0; 
+
+        for (int i = 0; i < n; i++) {
+            char expected = (i % 2 == 0) ? '0' : '1'; 
+            if (s.charAt(i) != expected) cnt++;
         }
-        for(int i = 1 ; i <=n;i++ ){
-            if(i%2 == 0) second.append('1');
-            else second.append('0');
-        }
-        int ans1 = 0;
-        int ans2 = 0;
-        for(int i = 0 ; i < n ;i++){
-            if(first.charAt(i)!=s.charAt(i)) ans1++;
-            if(second.charAt(i) != s.charAt(i)) ans2++;
-        }
-        return Math.min(ans1,ans2);
+
+        return Math.min(cnt, n - cnt);
     }
 }
