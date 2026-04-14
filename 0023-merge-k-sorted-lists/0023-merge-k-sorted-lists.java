ce1c58028872606982ca6a0f072b1a644d631caa
PAWAR SUDHARSHAN <pawarsudharshan47@gmail.com>
2026-04-14 17:45:58 +0530

    Time: 5 ms (41.06%), Space: 46.4 MB (96.09%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -1,39 +1,23 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         if (lists == null || lists.length == 0) return null;
 
-        ListNode dummy = new ListNode(-1);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (ListNode head : lists) {
+            while (head != null) {
+                pq.offer(head.val);
+                head = head.next;
+            }
+        }
+
+        if (pq.isEmpty()) return null;
+
+        ListNode dummy = new ListNode(0);
         ListNode tail = dummy;
 
-        while (true) {
-            int minIndex = -1;
-            int minVal = Integer.MAX_VALUE;
-
-            // Find index of list whose current node has minimum value
-            for (int i = 0; i < lists.length; i++) {
-                if (lists[i] != null && lists[i].val < minVal) {
-                    minVal = lists[i].val;
-                    minIndex = i;
-                }
-            }
-
-            // All lists exhausted
-            if (minIndex == -1) break;
-
-            // Attach that node and move its pointer
-            tail.next = lists[minIndex];
+        while (!pq.isEmpty()) {
+            tail.next = new ListNode(pq.poll());
             tail = tail.next;
-            lists[minIndex] = lists[minIndex].next;
         }
 
         return dummy.next;
