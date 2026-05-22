2b834fba5a8a78a572717855af2f43c22b289a13
PAWAR SUDHARSHAN <pawarsudharshan47@gmail.com>
2026-05-22 10:18:17 +0530

    Time: 9 ms (66.38%), Space: 49 MB (83.36%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
@@ -1,40 +1,38 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+import java.util.*;
+
 class Solution {
     public int maxLevelSum(TreeNode root) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        dfs(root,1,map);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
 
-        int level  = 1;
-        int sum = map.get(1);
-        for(int l : map.keySet()){
-            int s = map.get(l);
-            if(s > sum || s == sum && l < level){
-                sum = s;
-                level = l;
+        int level = 1;
+        int maxLevel = 1;
+        int maxSum = Integer.MIN_VALUE;
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            int sum = 0;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                sum += node.val;
+
+                if (node.left != null) {
+                    q.offer(node.left);
+                }
+                if (node.right != null) {
+                    q.offer(node.right);
+                }
             }
-        }
-        return level;
-    }
 
-    public static void dfs(TreeNode root , int level , HashMap<Integer,Integer> map){
-        if(root == null) return;
-        map.put(level , map.getOrDefault(level , 0)+root.val);
-        dfs(root.left , level+1,map);
-        dfs(root.right,level+1 ,map);
+            if (sum > maxSum) {
+                maxSum = sum;
+                maxLevel = level;
+            }
+
+            level++;
+        }
+
+        return maxLevel;
     }
-   
 }
