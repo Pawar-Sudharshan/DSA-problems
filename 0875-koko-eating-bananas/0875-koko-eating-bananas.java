af56541bdc68722b6f3b39d51d2cf935a16272a8
PAWAR SUDHARSHAN <pawarsudharshan47@gmail.com>
2026-06-18 23:48:02 +0530

    Time: 7 ms (81.02%), Space: 47.6 MB (85.7%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -3,31 +3,22 @@ class Solution {
         int left = 1;
         int right = 0;
         for (int p : piles) {
-            right = Math.max(right, p);   // max pile size
+            right = Math.max(right, p);   
         }
-
         int ans = right;
-
         while (left <= right) {
-            int mid = left + (right - left) / 2; // current speed k
+            int mid = left + (right - left) / 2; 
             long hours = 0;
-
-            // compute total hours needed with speed = mid
             for (int p : piles) {
-                // ceil(p / mid) without using double
                 hours += (p + mid - 1) / mid;
             }
-
             if (hours <= h) {
-                // can finish in time, try smaller speed
                 ans = mid;
                 right = mid - 1;
             } else {
-                // need more speed
                 left = mid + 1;
             }
         }
-
         return ans;
     }
 }
