ffe4b06ebb0d1d2e36c34c144200f6c187ef02d7
PAWAR SUDHARSHAN <pawarsudharshan47@gmail.com>
2026-07-11 09:09:21 +0530

    Time: 0 ms (100%), Space: 42.4 MB (17.47%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -2,12 +2,16 @@ class Solution {
     public int climbStairs(int n) {
         if (n == 1) return 1;
         if (n == 2) return 2;
-        int[] dp = new int[n + 1];
-        dp[1] = 1;
-        dp[2] = 2;
+        // int[] dp = new int[n + 1];
+        int p1  = 1;
+        // dp[2] = 2;
+        int p2 = 2;
         for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            // dp[i] = dp[i - 1] + dp[i - 2];
+            int temp = p2;
+            p2 = p2+p1;
+            p1 = temp;
         }
-        return dp[n];
+        return p2;
     }
 }
