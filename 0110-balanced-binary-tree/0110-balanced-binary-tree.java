7fc644c272e668abf5d9fe718da46fdef3e1cf06
PAWAR SUDHARSHAN <pawarsudharshan47@gmail.com>
2026-07-12 00:51:47 +0530

    Time: 0 ms (100%), Space: 45.5 MB (59.43%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -0,0 +1,1 @@
+class Solution{public boolean isBalanced(TreeNode r){return getHeight(r)!=-1;}int getHeight(TreeNode n){if(n==null)return 0;int l=getHeight(n.left);if(l==-1)return -1;int r=getHeight(n.right);if(r==-1)return -1;if(Math.abs(l-r)>1)return -1;return Math.max(l,r)+1;}}
