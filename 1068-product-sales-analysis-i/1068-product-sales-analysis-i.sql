64ce0f9069cf99ca7dfba078230849262b630b26
PAWAR SUDHARSHAN <pawarsudharshan47@gmail.com>
2026-08-18 21:40:29 +0530

    Time: 1357 ms (49.39%), Space: 0B (100%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-select p.product_name  , s.year , s.price 
-from sales s left join product p on p.product_id =s.product_id; 
+select p.product_name as product_name , s.year as year , s.price as price
+from sales s left join product p on s.product_id  = p.product_id ;
