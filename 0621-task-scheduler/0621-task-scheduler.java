9c69889169a160ab395f84fdd0a0f9557562ef59
PAWAR SUDHARSHAN <pawarsudharshan47@gmail.com>
2026-08-24 18:45:47 +0530

    Time: 2 ms (97.81%), Space: 48.4 MB (22.22%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,43 +1,75 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        int[] cnt = new int[26];
+        // int[] cnt = new int[26];
+        // int max = 0;
+        // for (char ch : tasks) {
+        //     cnt[ch - 'A']++;
+        //     max = Math.max(max,cnt[ch-'A']);
+        // }
+        // int cntMax = 0;
+        // for(int i : tasks) if(i == max) cntMax++;
+        // int time = (max-1)*(n+1) + cntMax;
+        // return Math.max(time,tasks.length);
+        // PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
 
-        for (char ch : tasks) {
-            cnt[ch - 'A']++;
+        // for (int i : cnt) {
+        //     if (i != 0) {
+        //         pq.offer(i);
+        //     }
+        // }
+
+        // int time = 0;
+        // Map<Integer, List<Integer>> map = new HashMap<>();
+
+        // while (!pq.isEmpty() || !map.isEmpty()) {
+        //     time++;
+
+        //     if (!pq.isEmpty()) {
+        //         int top = pq.poll();
+        //         top--;
+
+        //         if (top > 0) {
+        //             map.computeIfAbsent(time + n, key -> new ArrayList<>()).add(top);
+        //         }
+        //     }
+
+        //     if (map.containsKey(time)) {
+        //         for (int value : map.get(time)) {
+        //             pq.offer(value);
+        //         }
+
+        //         map.remove(time);
+        //     }
+        // }
+
+        // return time;
+        // int max = 0;
+         int[] freq = new int[26];
+
+        for (char task : tasks) {
+            freq[task - 'A']++;
         }
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        // Step 2: Find maximum frequency
+        int maxFreq = 0;
 
-        for (int i : cnt) {
-            if (i != 0) {
-                pq.offer(i);
+        for (int f : freq) {
+            maxFreq = Math.max(maxFreq, f);
+        }
+
+        // Step 3: Count how many tasks have maximum frequency
+        int countMaxFreq = 0;
+
+        for (int f : freq) {
+            if (f == maxFreq) {
+                countMaxFreq++;
             }
         }
 
-        int time = 0;
-        Map<Integer, List<Integer>> map = new HashMap<>();
+        // Step 4: Calculate minimum intervals
+        int result = (maxFreq - 1) * (n + 1) + countMaxFreq;
 
-        while (!pq.isEmpty() || !map.isEmpty()) {
-            time++;
-
-            if (!pq.isEmpty()) {
-                int top = pq.poll();
-                top--;
-
-                if (top > 0) {
-                    map.computeIfAbsent(time + n, key -> new ArrayList<>()).add(top);
-                }
-            }
-
-            if (map.containsKey(time)) {
-                for (int value : map.get(time)) {
-                    pq.offer(value);
-                }
-
-                map.remove(time);
-            }
-        }
-
-        return time;
+        // We cannot need fewer intervals than the number of tasks
+        return Math.max(tasks.length, result);
     }
 }
