5ee7c62d38861e6ae37ebba4d11bf0047ce13ec8
PAWAR SUDHARSHAN <pawarsudharshan47@gmail.com>
2026-09-07 21:05:35 +0530

    Time: 0 ms (100%), Space: 42.5 MB (94.37%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -3,13 +3,15 @@ class Solution {
         int n = nums.length;
         if(n==0) return 0;
         if(n==1) return nums[0];
-        int dp[] = new int[n+1]; 
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0] , nums[1]);
+        // int dp[] = new int[n+1]; 
+        int p1 = nums[0];
+        int p2 = Math.max(nums[0] , nums[1]);
         for(int i =2 ; i <n ;i++){
-            dp[i] = Math.max(nums[i]+dp[i-2] ,dp[i-1]);
+           int next = Math.max(nums[i]+p1 ,p2);
+           p1 = p2;
+           p2 = next;
         }
-        return dp[n-1];
+        return p2;
     }
     private static  int f(int idx,int[] nums , int[] dp){
         if(idx >= nums.length) return 0;
