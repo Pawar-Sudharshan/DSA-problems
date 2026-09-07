90e7876f004a5deb78d7444544464dade75f80ab
PAWAR SUDHARSHAN <pawarsudharshan47@gmail.com>
2026-09-07 21:03:00 +0530

    Time: 0 ms (100%), Space: 42.5 MB (94.37%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,31 +1,19 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        int dp[] = new int[n+1]; 
         dp[0] = nums[0];
-        for(int i = 1; i < n ;i++){
-            int take = nums[i] +( (i>1)?dp[i-2]:0);
-            int non = dp[i-1];
-            dp[i]=Math.max(take,non);
+        dp[1] = Math.max(nums[0] , nums[1]);
+        for(int i =2 ; i <n ;i++){
+            dp[i] = Math.max(nums[i]+dp[i-2] ,dp[i-1]);
         }
-        return  dp[n-1];
+        return dp[n-1];
     }
-    private static int helper(int[] nums , int idx,int[] dp){
-        if(idx == 0) return nums[idx];
-        // if(idx < 0) return 0;
-        
-        int pick = nums[idx];
-        if(idx-2 >= 0 && dp[idx-2] == -1  ){
-            dp[idx-2] = helper(nums,idx-2,dp);
-        }
-        pick +=(idx-2 >= 0)?dp[idx-2]:0;
-
-        int nonPick = 0;
-        if(idx-1 >= 0 && dp[idx-1] == -1  ){
-            dp[idx-1] = helper(nums,idx-1,dp);
-        } 
-        nonPick = (idx-1 >= 0)?dp[idx-1]:0;
-        return Math.max(pick , nonPick);
+    private static  int f(int idx,int[] nums , int[] dp){
+        if(idx >= nums.length) return 0;
+        if(dp[idx] != -1) return dp[idx];
+        return dp[idx];
     }
 }
